e1791b10ada0ed369fdbcf7ad9171f9712953979
ArtemNaronov <a.naronov@bk.ru>
2021-11-15 20:48:57 +0400

    new test

---
+++ components/Content.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{ lang.title }}</h1>
+  </div>
+</template>
+
+<script>
+
+export default ({
+  data() {
+    return {}
+  },
+  computed: {
+    lang() {
+      return this.$store.getters.get_lang;
+    }
+  }
+})
+</script>
